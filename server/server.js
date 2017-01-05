9eea3ba2cb1c679924ced0f0080cf01d504b18c2
gwhitaker <garth.whitaker@metropolitan.co.za>
2017-01-05 12:06:57 +0200

    Updates for heroku capabilities

--- server/server.js
+++ server/server.js
@@ -1,15 +1,10 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
-
+const port = process.env.PORT ||3000;
 const app = express();
 app.use(express.static(publicPath));
-
-// app.get('/', (req, res) => {
-//     res.sendFile('index.html');
-// });
-
-app.listen(3000, () => {
-    console.log('Server started on port 3000.');
+app.listen(port, () => {
+    console.log(`Server started on port ${port}.`);
 })
 
